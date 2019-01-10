633f5230b30527a1b6b24609a84fd827ea2c9938
Lukasz Dubiel <lukasz.dubiel@digital.hmrc.gov.uk>
2019-01-10 11:08:44 +0000

    Add authorization to endpoints

--- app/uk/gov/hmrc/ofstedformsnotifications/controllers/OfstedNotifications.scala
+++ app/uk/gov/hmrc/ofstedformsnotifications/controllers/OfstedNotifications.scala
@@ -21,6 +21,7 @@ import java.time.format.DateTimeFormatter
 import javax.inject.{Inject, Named, Singleton}
 import play.api.libs.json.JsError
 import play.api.mvc._
+import uk.gov.hmrc.auth.core.{AuthConnector, AuthorisedFunctions}
 import uk.gov.hmrc.ofstedformsnotifications.client.{NotificationFacade, Reference}
 import uk.gov.hmrc.ofstedformsnotifications.{FormNotification, TemplateConfiguration}
 import uk.gov.hmrc.play.bootstrap.controller.FrontendController
@@ -31,54 +32,60 @@ import scala.concurrent.{ExecutionContext, Future}
 class OfstedNotifications @Inject()(mcc: MessagesControllerComponents,
                                     notifications: NotificationFacade,
                                     templates: TemplateConfiguration,
-                                    @Named("rejection-url") rejectionUrl: String)
-                                   (implicit executionContext: ExecutionContext) extends FrontendController(mcc) {
+                                    @Named("rejection-url") rejectionUrl: String,
+                                    val authConnector: AuthConnector)
+                                   (implicit executionContext: ExecutionContext) extends FrontendController(mcc) with AuthorisedFunctions {
 
   private val formatter = DateTimeFormatter.ofPattern("hh:mm a dd-MMMM-yyyy")
 
   val submission = Action.async(parse.json) { implicit request =>
-    request.body.validate[FormNotification].fold(
-      err => Future.successful(BadRequest(JsError.toJson(JsError(err)))),
-      notification => notifications.sendByEmail(
-        templates.submission,
-        notification.email,
-        Map("form-id" -> notification.id, "submission-time" -> formatter.format(notification.time)),
-        Reference(notification.id)
-      ).map(result => Ok(result.notificationId.asString))
-    )
+    authorised() {
+      request.body.validate[FormNotification].fold(
+        err => Future.successful(BadRequest(JsError.toJson(JsError(err)))),
+        notification => notifications.sendByEmail(
+          templates.submission,
+          notification.email,
+          Map("form-id" -> notification.id, "submission-time" -> formatter.format(notification.time)),
+          Reference(notification.id)
+        ).map(result => Ok(result.notificationId.asString))
+      )
+    }
   }
 
   val acceptance = Action.async(parse.json) { implicit request =>
-    request.body.validate[FormNotification].fold(
-      err => Future.successful(BadRequest(JsError.toJson(JsError(err)))),
-      notification => notifications.sendByEmail(
-        templates.acceptance,
-        notification.email,
-        Map(
-          "form-id" -> notification.id,
-          "acceptance-time" -> formatter.format(notification.time)
-        ),
-        Reference(notification.id)
-      ).map(result => Ok(result.notificationId.asString))
-    )
+    authorised() {
+      request.body.validate[FormNotification].fold(
+        err => Future.successful(BadRequest(JsError.toJson(JsError(err)))),
+        notification => notifications.sendByEmail(
+          templates.acceptance,
+          notification.email,
+          Map(
+            "form-id" -> notification.id,
+            "acceptance-time" -> formatter.format(notification.time)
+          ),
+          Reference(notification.id)
+        ).map(result => Ok(result.notificationId.asString))
+      )
+    }
   }
 
   val rejection = Action.async(parse.json) { implicit request =>
-    request.body.validate[FormNotification].fold(
-      err => Future.successful(BadRequest(JsError.toJson(JsError(err)))),
-      notification => notifications.sendByEmail(
-        templates.rejection,
-        notification.email,
-        Map(
-          "form-id" -> notification.id,
-          "rejection-time" -> formatter.format(notification.time),
-          "url" -> rejectionUrl
-        ),
-        Reference(notification.id)
-      ).map(result => Ok(result.notificationId.asString))
-    )
+    authorised() {
+      request.body.validate[FormNotification].fold(
+        err => Future.successful(BadRequest(JsError.toJson(JsError(err)))),
+        notification => notifications.sendByEmail(
+          templates.rejection,
+          notification.email,
+          Map(
+            "form-id" -> notification.id,
+            "rejection-time" -> formatter.format(notification.time),
+            "url" -> rejectionUrl
+          ),
+          Reference(notification.id)
+        ).map(result => Ok(result.notificationId.asString))
+      )
+    }
   }
-
 
 
 }
